59bebe45e6407e385a0d98ec114af2abe1cfdcc4
Chaithra <chaithrap140@gmail.com>
2023-07-13 06:43:13 +0400

    Update index.html and script.js

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ function uploadImage() {
     const input = document.getElementById('upload-input');
     const imageContainer = document.getElementById('image-container');
     const uploadedImage = document.getElementById('uploaded-image');
+    const downloadLink = document.getElementById('download-link');
 
     if (input.files && input.files[0]) {
         const reader = new FileReader();
 
         reader.onload = function(e) {
             uploadedImage.src = e.target.result;
-        }
+            downloadLink.href = e.target.result;
+        };
 
         reader.readAsDataURL(input.files[0]);
         imageContainer.style.display = 'block';
+        downloadLink.style.display = 'block';
     }
 }
